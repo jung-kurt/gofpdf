b3e554e5eaf9454cc02a9f09dc3aab9e132eb0ca
Kurt Jung <kurt.w.jung@code.google.com>
2013-08-05 10:42:40 -0400

    Make sure makefont utility has been built before running test

--- makefont/makefont_test.go
+++ makefont/makefont_test.go
@@ -7,8 +7,15 @@ import (
 )
 
 func TestMakefont(t *testing.T) {
+	var out []byte
+	var err error
 	const expect = "Font definition file successfully generated"
-	out, err := exec.Command("./makefont", "--dst=../font", "--embed",
+	// Make sure makefont utility has been built before generating font definition file
+	err = exec.Command("go", "build").Run()
+	if err != nil {
+		t.Fatal(err)
+	}
+	out, err = exec.Command("./makefont", "--dst=../font", "--embed",
 		"--enc=../font/cp1252.map", "../font/calligra.ttf").CombinedOutput()
 	if err != nil {
 		t.Fatal(err)
