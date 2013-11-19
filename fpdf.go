131de2d8072a8594ec6383d1ce2e58e18597210b
Kurt Jung <kurt.w.jung@code.google.com>
2013-11-19 08:41:45 -0500

    Bring new comments into lint conformance

--- fpdf.go
+++ fpdf.go
@@ -266,16 +266,16 @@ func (f *Fpdf) SetLeftMargin(margin float64) {
 	}
 }
 
-// Return the cell margin. This is the amount of space before and after the text
-// within a cell that's left blank, and is in units passed to New(). It defaults
-// to 1mm.
+// GetCellMargin returns the cell margin. This is the amount of space before
+// and after the text within a cell that's left blank, and is in units passed
+// to New(). It defaults to 1mm.
 func (f *Fpdf) GetCellMargin() float64 {
 	return f.cMargin
 }
 
-// Set the cell margin. This is the amount of space before and after the text
-// within a cell that's left blank, and is in units passed to New(). It defaults
-// to 1mm.
+// SetCellMargin set the cell margin. This is the amount of space before and
+// after the text within a cell that's left blank, and is in units passed to
+// New().
 func (f *Fpdf) SetCellMargin(margin float64) {
 	f.cMargin = margin
 }
