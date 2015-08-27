a213cdab820620d8ce3df2fc63a84a8e97eaf251
Jelmer Snoeck <jelmer.snoeck@gmail.com>
2015-08-27 08:21:52 +0100

    Use internal example package for testing.

--- contrib/httpimg/httpimg_test.go
+++ contrib/httpimg/httpimg_test.go
@@ -1,44 +1,10 @@
 package httpimg_test
 
 import (
-	"fmt"
 	"github.com/jung-kurt/gofpdf"
 	"github.com/jung-kurt/gofpdf/contrib/httpimg"
-	"os"
-	"path/filepath"
+	"github.com/jung-kurt/gofpdf/internal/example"
 )
-
-const (
-	cnGofpdfDir  = "./"
-	cnExampleDir = cnGofpdfDir + "/pdf"
-)
-
-func init() {
-	cleanup()
-}
-
-func cleanup() {
-	filepath.Walk(cnExampleDir,
-		func(path string, info os.FileInfo, err error) (reterr error) {
-			if path[len(path)-4:] == ".pdf" {
-				os.Remove(path)
-			}
-			return
-		})
-}
-
-func exampleFilename(baseStr string) string {
-	return filepath.Join(cnExampleDir, baseStr+".pdf")
-}
-
-func summary(err error, fileStr string) {
-	if err == nil {
-		fileStr = filepath.ToSlash(fileStr)
-		fmt.Printf("Successfully generated %s\n", fileStr)
-	} else {
-		fmt.Println(err)
-	}
-}
 
 func ExampleRegister() {
 	pdf := gofpdf.New("", "", "", "")
@@ -50,9 +16,9 @@ func ExampleRegister() {
 	httpimg.Register(pdf, url, "")
 	pdf.Image(url, 100, 100, 20, 20, false, "", 0, "")
 
-	fileStr := exampleFilename("contrib_httpimg_Register")
+	fileStr := example.Filename("contrib_httpimg_Register")
 	err := pdf.OutputFileAndClose(fileStr)
-	summary(err, fileStr)
+	example.Summary(err, fileStr)
 	// Output:
-	// Successfully generated pdf/contrib_httpimg_Register.pdf
+	// Successfully generated ../../pdf/contrib_httpimg_Register.pdf
 }
