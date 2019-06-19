fe829f165f4138c434c358c57eb9dcffc331571c
Ivan Korostelev <ivan.korostelev@onetwotrip.com>
2019-06-19 12:56:05 +0300

    fpdi.Importer wrapper for thread-safe usage

--- contrib/gofpdi/gofpdi.go
+++ contrib/gofpdi/gofpdi.go
@@ -17,6 +17,18 @@ type gofpdiPdf interface {
 	SetError(err error)
 }
 
+type FpdiImporter struct {
+	fpdi *realgofpdi.Importer
+	fpdf gofpdiPdf
+}
+
+func NewFpdiImporter(f gofpdiPdf) *FpdiImporter {
+	return &FpdiImporter{
+		fpdi: realgofpdi.NewImporter(),
+		fpdf: f,
+	}
+}
+
 // ImportPage imports a page of a PDF file with the specified box (/MediaBox,
 // /TrimBox, /ArtBox, /CropBox, or /BleedBox). Returns a template id that can
 // be used with UseImportedTemplate to draw the template onto the page.
@@ -60,4 +72,49 @@ func UseImportedTemplate(f gofpdiPdf, tplid int, x float64, y float64, w float64
 	tplName, scaleX, scaleY, tX, tY := fpdi.UseTemplate(tplid, x, y, w, h)
 
 	f.UseImportedTemplate(tplName, scaleX, scaleY, tX, tY)
+}
+
+// ImportPage imports a page of a PDF file with the specified box (/MediaBox,
+// /TrimBox, /ArtBox, /CropBox, or /BleedBox). Returns a template id that can
+// be used with UseImportedTemplate to draw the template onto the page.
+func (i *FpdiImporter) ImportPage(sourceFile string, pageno int, box string) int {
+	// Set source file for fpdi
+	i.fpdi.SetSourceFile(sourceFile)
+
+	// Import page
+	tpl := i.fpdi.ImportPage(pageno, box)
+
+	// Import objects into current pdf document
+	// Unordered means that the objects will be returned with a sha1 hash instead of an integer
+	// The objects themselves may have references to other hashes which will be replaced in ImportObjects()
+	tplObjIDs := i.fpdi.PutFormXobjectsUnordered()
+
+	// Set template names and ids (hashes) in gofpdf
+	i.fpdf.ImportTemplates(tplObjIDs)
+
+	// Get a map[string]string of the imported objects.
+	// The map keys will be the ID of each object.
+	imported := i.fpdi.GetImportedObjectsUnordered()
+
+	// Import gofpdi objects into gofpdf
+	i.fpdf.ImportObjects(imported)
+
+	// Get a map[string]map[int]string of the object hashes and their positions within each object,
+	// to be replaced with object ids (integers).
+	importedObjPos := i.fpdi.GetImportedObjHashPos()
+
+	// Import gofpdi object hashes and their positions into gopdf
+	i.fpdf.ImportObjPos(importedObjPos)
+
+	return tpl
+}
+
+// UseImportedTemplate draws the template onto the page at x,y. If w is 0, the
+// template will be scaled to fit based on h. If h is 0, the template will be
+// scaled to fit based on w.
+func (i *FpdiImporter) UseImportedTemplate(tplid int, x float64, y float64, w float64, h float64) {
+	// Get values from fpdi
+	tplName, scaleX, scaleY, tX, tY := i.fpdi.UseTemplate(tplid, x, y, w, h)
+
+	i.fpdf.UseImportedTemplate(tplName, scaleX, scaleY, tX, tY)
 }
